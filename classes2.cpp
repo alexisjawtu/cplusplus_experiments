62f00da44b39ce41e81a512881d1980ba8c38a38
Alexis Jawtuschenko <alexis.jawtuschenko@gmail.com>
2024-08-07 19:50:11 -0300

    statics

--- classes2.cpp
+++ classes2.cpp
@@ -18,63 +18,89 @@ template <class T> void print(T data)
 }
 
 class CVector {
-	public:
-		float x;
-		float y;
+  public:    
+    float x;
+    float y;
 
-		CVector () {};
-		CVector (float a, float b) : x(a), y(b) {};
+    CVector () {};
+    CVector (float a, float b) : x(a), y(b) {};
 
-		CVector& operator= (const CVector&);
-		CVector  operator+ (const CVector&);
+    CVector& operator= (const CVector&);
+    CVector  operator+ (const CVector&);
 
-		bool isitme (CVector& v);
+    bool isitme (CVector& v);
 
-		friend ostream& operator<< (ostream& o, const CVector& v);
+    friend ostream& operator<< (ostream& o, const CVector& v);
 };
 
 CVector& CVector::operator= (const CVector& v) {
-	this -> x = v.x;
-	this -> y = v.y;
-	return *this;
+    this -> x = v.x;
+    this -> y = v.y;
+    return *this;
 }
 
 CVector CVector::operator+ (const CVector& v) {
-	return CVector (this -> x + v.x, this -> y + v.y);
+    return CVector (this -> x + v.x, this -> y + v.y);
 }
 
 bool CVector::isitme (CVector& v) {
-	return (this == &v);
+    return (this == &v);
 }
 
 ostream& operator<< (ostream& o, const CVector& v) {
-	return o << "(" << v.x << ", " << v.y << ")";
+    return o << "(" << v.x << ", " << v.y << ")";
 }
+
+
+/*
+    static used to count class objects allocated
+*/
+class Dummy {
+  public:
+    static int n;
+    Dummy () {
+        n++;
+    }
+};
+
+int Dummy::n = 0;
 
 int main(int argc, char const *argv[])
 {
-	CVector u, v, w;
-	
-	u.x = 1.3;
-	u.y = -2;
+    CVector u, v, w;
+    
+    u.x = 1.3;
+    u.y = -2;
 
-	v.x = 6;
-	v.y = 0.7;
+    v.x = 6;
+    v.y = 0.7;
 
-	w = u + v;
+    w = u + v;
 
-	print(w);
+    print(w);
 
-	print(w.operator+(u));
+    print(w.operator+(u));
 
-	print(u.isitme(w));
-	print(u.isitme(u));
+    print(u.isitme(w));
+    print(u.isitme(u));
 
-	CVector * p = &v;
+    CVector * p = &v;
 
-	print(p -> isitme(v));
+    print(p -> isitme(v));
 
-	return 0;
+    print("==========================================");
+    
+    print(Dummy::n);
+    Dummy a;
+    print(Dummy::n);
+    Dummy b[5];
+    print(a.n);
+    Dummy * c;
+    print(Dummy::n);
+    c = new Dummy;
+    print(b[2].n);
+
+    return 0;
 }
-
-CONTINUE at classesII.Static Members
+    
+// CONTINUE at classesII.{Const members}
