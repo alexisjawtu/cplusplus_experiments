18d8dedbadeb2fcbefb9f310d3c0e52fd711bc23
Alexis Jawtuschenko <alexis.jawtuschenko@gmail.com>
2024-08-14 20:23:54 -0300

    From destructor

--- special-members.cpp
+++ special-members.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 
 using namespace std;
@@ -10,8 +11,30 @@ class Example {
   	void accumulate (int x) { total += x; }
 };
 
+class Example3 {
+	string data;
+  public:
+  	Example3 () {}
+  	Example3 (const string& str) {
+  		this -> data = str;
+  	}
+
+  	// getters use to be const ... () const
+  	const string& content () const {
+  		return data;
+  	}
+};
+
 int main () {
 	Example ex;
-	
+	cout << "success" << endl;
+
+	Example3 ex3 ("boris");
+
+	cout << ex3.content() << endl;
+
 	return 0;
 }
+
+
+//CONTINUE at: special members.Destructor
