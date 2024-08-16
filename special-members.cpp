df50d7f9858bc0afbc3cd9b4cdc3b2c03b4acbd6
Alexis Jawtuschenko <alexis.jawtuschenko@gmail.com>
2024-08-16 15:31:49 -0300

    Destructor

--- special-members.cpp
+++ special-members.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "_tools_.h"
 
 
 using namespace std;
@@ -25,16 +26,32 @@ class Example3 {
   	}
 };
 
+class Example4 {
+    string* ptr;
+  public:
+    Example4 () {
+        ptr = new string ("");
+    }
+    Example4 (const string& str) {
+        ptr = new string (str);
+    }
+    ~Example4 () {
+        delete ptr;
+        cout << "HEY" << "\n";
+    }
+    const string& content () const {
+        return *ptr;
+    }
+};
+
 int main () {
-	Example ex;
-	cout << "success" << endl;
 
-	Example3 ex3 ("boris");
+    Example4 ex4 = Example4();
+    Example4 ex5 = Example4("alo");
 
-	cout << ex3.content() << endl;
-
+    cout << ex5.content() << "\n";
 	return 0;
 }
 
 
-//CONTINUE at: special members.Destructor
+//CONTINUE at: special members.copy constructor
