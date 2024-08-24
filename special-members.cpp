59840191dc918b8d712886374ec96413e9724d2a
Alexis Jawtuschenko <alexis.jawtuschenko@gmail.com>
2024-08-24 19:35:55 -0300

    Finish copy assignment

--- special-members.cpp
+++ special-members.cpp
@@ -33,16 +33,38 @@ class Example4 {
     Example4 () {
         this -> ptr = new string ("");
     }
+
     Example4 (const string& str) {
         this -> ptr = new string (str);
     }
+
     Example4 (const Example4& e) {
         this -> ptr = new string(e.content());
     }
+    
     ~Example4 () {
         delete this -> ptr;
         cout << "DELETED " << this << "\n";
     }
+    
+    Example4& operator= (const Example4& e)
+    {
+        print("running operator= overload");
+        delete this -> ptr;  // delete the current object pointed to.
+        this -> ptr = new string (e.content());  // copy and allocate
+        return *this;
+    }
+
+    // /*
+    //     Optional: the data string is not constant so we 
+    //     could modify it in place.
+    // */
+    // Example4& operator= (const Example4& e)
+    // {
+    //     *(this -> ptr) = e.content();
+    //     return *this;
+    // }
+    
     const string& content () const {
         return *ptr;
     }
@@ -50,12 +72,17 @@ class Example4 {
 
 int main () {
 
+    print("line 1");
     Example4 ex4 = Example4();
+    print("line 2");
     Example4 ex5 = Example4("alo");
+    print("line 3");
     Example4 ex6 = Example4("sha");
+    print("line 4");
     Example4 ex7 = ex5;  // this line is merely a construction by copy. 
 
-    ex4 = ex5;  // this line leads to a segmentation fault because of the assignment issue.
+    print("line 5");
+    ex4 = ex5;  // Hadn't we written an operator=, this line would lead us to a segmentation fault because of the assignment issue.
                 // maybe this performs a shallow copy, which is not suitable for classes
                 // that use dynamic memory.
 
@@ -69,4 +96,4 @@ int main () {
 }
 
 
-//CONTINUE at: special members.Copy assignment (insistir!)
+// CONTINUE at: {special members}.{Move constructor and assignment}
