eba76111963e0c1b9438683517ee1d9045f93eab
Alexis Jawtuschenko <alexis.jawtuschenko@gmail.com>
2024-09-04 19:01:54 -0300

    Experiment with multiple inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -5,6 +5,23 @@
 using namespace std;
 
 
+class Output {
+
+public:
+
+    /*
+        static is something that is member of the class itself
+        we don't need any instance to call and use a static member
+    */
+    static void show_on_screen (int i); 
+
+};
+
+void Output::show_on_screen (int i)
+{
+    cout << i << "\n";
+}
+
 class Polygon {
 
 protected:
@@ -14,18 +31,33 @@ protected:
 
 public:
 
+	Polygon ()
+	{
+		print("default constr: I am being executed");
+	}
+
+    Polygon (int h, int w)
+    {
+        print("parameter constr: redirection of the constructor");
+        this -> height = h;
+        this -> width  = w;
+    }
+
 	void set_values (int h, int w) 
 	{
 		this -> height = h;
 		this -> width  = w;
 	}
 
-	//area()
 };
 
-class Rectangle: public Polygon {
+class Rectangle: public Polygon, public Output {
 
 public:
+
+    Rectangle () {};
+
+    Rectangle (int h, int w) : Polygon (h, w) {};
 
 	int area ()
 	{
@@ -34,11 +66,15 @@ public:
 
 };
 
-class Triangle: public Polygon {
+class Triangle: public Polygon, public Output {
 
 public:
+    
+    Triangle () {};
 
-	int area ()
+    Triangle (int h, int w) : Polygon (h, w) {};
+
+	float area ()
 	{
 		return (this -> height) * (this -> width)/2;
 	}
@@ -47,13 +83,38 @@ public:
 
 int main ()
 {
+	/*
+        unless otherwise written the derived object is built
+        by the default constructor of the base class
+
+        Se puede redireccionar el llamado del constructor antepasado
+        usando la signatura del contructor que queramos llamar
+        de la base_class
+
+        ej:
+
+            Triangle (int height, int width) : Polygon (height, width) { ... };
+
+	*/
 	Triangle t0;
 	Rectangle r0;
 
 	t0.set_values(3,4);
 	r0.set_values(3,4);
 
-	printf("areas %d and %d\n", t0.area(), r0.area());
+	printf("areas %f and %d\n", t0.area(), r0.area());
+
+    Triangle t1 (8, 7);
+
+    printf("area t1 %f\n", t1.area());
+
+    Rectangle rect (5, 5);
+    Triangle  trgl (5, 5);
+
+    rect.show_on_screen(rect.area());
+    // static call:
+    Triangle::show_on_screen(trgl.area());
 
 	return 0;
+    
 }
