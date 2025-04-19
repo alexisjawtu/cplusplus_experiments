e48937ecb4a20adc2479a09846f731862b0af76e
alexis <alexis.jawtuschenko@gmail.com>
2025-04-19 20:39:07 -0300

    WIP

--- drill3.cpp
+++ drill3.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+
+
 using namespace std;
 
-SEGUIR ACA: test y debug del drill 3 [7] :)
 
 double to_mtrs (string u)
 {
     if (u == "cm")
-        return 1/100;
+        return 1.0/100;
     else if (u == "ft")
         return 12 * 0.0254;
     else if (u == "in")
@@ -15,47 +18,71 @@ double to_mtrs (string u)
     return 1;
 }
 
+bool illegal(string u)
+{
+    return (u != "cm") && (u != "ft") && (u != "in") && (u != "m");
+}
+
 int main ()
 { 
+    int count {0};
+    double num {0};
+    double sum {0};
     string unit;
-    double num;
+
+    vector <double> vd;
+
+    cout << "cm ft in m\n";
 
     cin >> num;
     cin >> unit;
 
-    num *= to_mtrs(unit);
+    while (illegal(unit))
+    {
+        cout << "illegal unit. try again\n";
+        cin >> num;
+        cin >> unit;
+    }
 
-    /*
-    if (unit == "cm")
-        num /= 100;
-    else if (unit == "ft")
-        num *= 12 * 0.0254;
-    else if (unit == "in")
-        num =* 0.0254;
-    */
+    num *= to_mtrs(unit);
+    ++count;
+    sum += num;
+    vd.push_back(num);
 
     double min_so_far {num};
     double max_so_far {num};
 
-    cout << num << "mtrs. max so far" << '\n';
-    cout << num << "mtrs. min so far" << '\n';
+    cout << num << " mtrs. max so far" << '\n';
+    cout << num << " mtrs. min so far" << '\n';
 
     while (cin >> num && cin >> unit)
     {
-        if (true)
+        while (illegal(unit))
         {
-            num *= to_mtrs(unit);
+            cout << "illegal unit. try again\n";
+            cin >> num;
+            cin >> unit;
+        }
 
-            if (num > max_so_far)
-            {
-                max_so_far = num;
-                cout << "mtrs. max so far" << '\n';
-            }
-            else if (num < min_so_far)
-            {
-                min_so_far = num;
-                cout << "mtrs. min so far" << '\n';
-            }
+        num *= to_mtrs(unit);
+        ++count;
+        sum += num;
+        vd.push_back(num);
+
+        if (num > max_so_far)
+        {
+            max_so_far = num;
+            cout << num << " mtrs. max so far" << '\n';
+        }
+        else if (num < min_so_far)
+        {
+            min_so_far = num;
+            cout << num << " mtrs. min so far" << '\n';
         }
     }
+    cout << "sum " << sum << ". count " << count << "\n";
+    ranges::sort(vd);
+
+    for (auto v: vd)
+        cout << '\t' << v << " mtrs\n";
 }
--- todr3.cpp
+++
@@ -1,9 +0,0 @@
-
-double min_so_far {0};
-double max_so_far {0};
-
-min_so_far = min_so_far > num ? num : min_so_far;
-max_so_far = max_so_far < num ? num : max_so_far;
-
-cout << "min_so_far" << min_so_far << '\n';
-cout << "max_so_far" << max_so_far << '\n';
