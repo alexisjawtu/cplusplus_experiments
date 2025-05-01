27c5b97e52ac82da45a7efd5e00d24d2f3b4ce23
alexis <alexis.jawtuschenko@gmail.com>
2025-05-01 18:31:47 -0300

    WIP

---
+++ ppp3/calculator/calculator.cpp
@@ -0,0 +1,77 @@
+#include "PPPheaders.h"
+
+
+using namespace std;
+
+
+class Token
+{
+    public:
+        char kind;
+        double value;
+        Token (char k): kind{k}, value{0.0} {}
+        Token (char k, double v): kind{k}, value{v} {}
+};
+
+
+Token get_token()
+{
+    return Token {'*'};
+}
+
+
+vector<Token> tok;
+
+
+int main ()
+{
+    while (cin)
+    {
+        Token t = get_token();
+        tok.push_back(t);
+    }
+}
+
+
+/*
+int first_version_main ()
+{
+    cout << "Expression (x to end) ";
+    int lval {0};
+    int rval {0};
+    cin >> lval;
+    if (!cin)
+        error ("missing 1st. operand");
+
+    for (char op; cin >> op; )
+    {
+        if (op != 'x')
+            cin >> rval;
+        if (!cin)
+            error ("missing 2nd. operand");
+
+        switch (op)
+        {
+            case '+':
+                lval += rval;
+                break;
+            case '-':
+                lval -= rval;
+                break;
+            case '*':
+                lval *= rval;
+                break;
+            case '/':
+                lval /= rval;
+                break;
+            default:
+                cout << "Result " << lval << '\n';
+                return 0;
+        }
+    }
+
+    error ("wrong expression");
+
+    return 0;
+}
+*/
